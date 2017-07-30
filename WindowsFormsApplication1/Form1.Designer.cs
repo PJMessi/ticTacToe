d4328655a571944dc3fcff410980b8f7644f935c
Prajwal Shrestha <prajwal.ansa@icloud.com>
2017-07-30 15:38:34 +0545

    did the initial work

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.a11 = new System.Windows.Forms.TextBox();
+            this.a12 = new System.Windows.Forms.TextBox();
+            this.a13 = new System.Windows.Forms.TextBox();
+            this.a21 = new System.Windows.Forms.TextBox();
+            this.a22 = new System.Windows.Forms.TextBox();
+            this.a23 = new System.Windows.Forms.TextBox();
+            this.a31 = new System.Windows.Forms.TextBox();
+            this.a32 = new System.Windows.Forms.TextBox();
+            this.a33 = new System.Windows.Forms.TextBox();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // a11
+            // 
+            this.a11.Location = new System.Drawing.Point(37, 57);
+            this.a11.Name = "a11";
+            this.a11.Size = new System.Drawing.Size(25, 20);
+            this.a11.TabIndex = 0;
+            this.a11.TextChanged += new System.EventHandler(this.a11_TextChanged);
+            // 
+            // a12
+            // 
+            this.a12.Location = new System.Drawing.Point(68, 57);
+            this.a12.Name = "a12";
+            this.a12.Size = new System.Drawing.Size(25, 20);
+            this.a12.TabIndex = 1;
+            this.a12.TextChanged += new System.EventHandler(this.a12_TextChanged);
+            // 
+            // a13
+            // 
+            this.a13.Location = new System.Drawing.Point(99, 57);
+            this.a13.Name = "a13";
+            this.a13.Size = new System.Drawing.Size(25, 20);
+            this.a13.TabIndex = 2;
+            this.a13.TextChanged += new System.EventHandler(this.a13_TextChanged);
+            // 
+            // a21
+            // 
+            this.a21.Location = new System.Drawing.Point(37, 83);
+            this.a21.Name = "a21";
+            this.a21.Size = new System.Drawing.Size(25, 20);
+            this.a21.TabIndex = 3;
+            this.a21.TextChanged += new System.EventHandler(this.a21_TextChanged);
+            // 
+            // a22
+            // 
+            this.a22.Location = new System.Drawing.Point(68, 83);
+            this.a22.Name = "a22";
+            this.a22.Size = new System.Drawing.Size(25, 20);
+            this.a22.TabIndex = 4;
+            this.a22.TextChanged += new System.EventHandler(this.a22_TextChanged);
+            // 
+            // a23
+            // 
+            this.a23.Location = new System.Drawing.Point(99, 83);
+            this.a23.Name = "a23";
+            this.a23.Size = new System.Drawing.Size(25, 20);
+            this.a23.TabIndex = 5;
+            this.a23.TextChanged += new System.EventHandler(this.a23_TextChanged);
+            // 
+            // a31
+            // 
+            this.a31.Location = new System.Drawing.Point(37, 109);
+            this.a31.Name = "a31";
+            this.a31.Size = new System.Drawing.Size(25, 20);
+            this.a31.TabIndex = 6;
+            this.a31.TextChanged += new System.EventHandler(this.a31_TextChanged);
+            // 
+            // a32
+            // 
+            this.a32.Location = new System.Drawing.Point(68, 109);
+            this.a32.Name = "a32";
+            this.a32.Size = new System.Drawing.Size(25, 20);
+            this.a32.TabIndex = 7;
+            this.a32.TextChanged += new System.EventHandler(this.a32_TextChanged);
+            // 
+            // a33
+            // 
+            this.a33.Location = new System.Drawing.Point(99, 109);
+            this.a33.Name = "a33";
+            this.a33.Size = new System.Drawing.Size(25, 20);
+            this.a33.TabIndex = 8;
+            this.a33.TextChanged += new System.EventHandler(this.a33_TextChanged);
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Location = new System.Drawing.Point(65, 151);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(0, 13);
+            this.lblresult.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(169, 196);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.a33);
+            this.Controls.Add(this.a32);
+            this.Controls.Add(this.a31);
+            this.Controls.Add(this.a23);
+            this.Controls.Add(this.a22);
+            this.Controls.Add(this.a21);
+            this.Controls.Add(this.a13);
+            this.Controls.Add(this.a12);
+            this.Controls.Add(this.a11);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox a11;
+        private System.Windows.Forms.TextBox a12;
+        private System.Windows.Forms.TextBox a13;
+        private System.Windows.Forms.TextBox a21;
+        private System.Windows.Forms.TextBox a22;
+        private System.Windows.Forms.TextBox a23;
+        private System.Windows.Forms.TextBox a31;
+        private System.Windows.Forms.TextBox a32;
+        private System.Windows.Forms.TextBox a33;
+        private System.Windows.Forms.Label lblresult;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,72 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void checker()
+        {
+            bool one, two, three, four, five, six, seven, eight;
+
+            one = (a11.Text == "o" && a22.Text == "o" && a33.Text == "o") || (a11.Text == "x" && a22.Text == "x" && a33.Text == "x");
+            two = (a31.Text == "o" && a22.Text == "o" && a13.Text == "o") || (a31.Text == "x" && a22.Text == "x" && a13.Text == "x");
+
+            three = (a11.Text == "o" && a12.Text == "o" && a13.Text == "o") || (a11.Text == "x" && a12.Text == "x" && a13.Text == "x");
+            four = (a21.Text == "o" && a22.Text == "o" && a23.Text == "o") || (a21.Text == "x" && a22.Text == "x" && a23.Text == "x");
+            five = (a31.Text == "o" && a32.Text == "o" && a33.Text == "o") || (a31.Text == "x" && a32.Text == "x" && a33.Text == "x");
+
+            six = (a11.Text == "o" && a21.Text == "o" && a31.Text == "o") || (a11.Text == "x" && a21.Text == "x" && a31.Text == "x");
+            seven = (a12.Text == "o" && a22.Text == "o" && a32.Text == "o") || (a12.Text == "x" && a22.Text == "x" && a32.Text == "x");
+            eight = (a13.Text == "o" && a23.Text == "o" && a33.Text == "o") || (a13.Text == "x" && a23.Text == "x" && a33.Text == "x");
+
+
+            if (one || two || three || four || five || six || seven || eight)
+            {
+                lblresult.Text = "game won";
+            }
+        }
+
+        private void a11_TextChanged(object sender, EventArgs e)
+        {
+            checker();
+        }
+
+        private void a12_TextChanged(object sender, EventArgs e)
+        {
+            checker();
+        }
+
+        private void a13_TextChanged(object sender, EventArgs e)
+        {
+            checker();
+        }
+
+        private void a21_TextChanged(object sender, EventArgs e)
+        {
+            checker();
+        }
+
+        private void a22_TextChanged(object sender, EventArgs e)
+        {
+            checker();
+        }
+
+        private void a23_TextChanged(object sender, EventArgs e)
+        {
+            checker();
+        }
+
+        private void a31_TextChanged(object sender, EventArgs e)
+        {
+            checker();
+        }
+
+        private void a32_TextChanged(object sender, EventArgs e)
+        {
+            checker();
+        }
+
+        private void a33_TextChanged(object sender, EventArgs e)
+        {
+            checker();
+        }
     }
 }
